ca6ca34627a083082f4abee528d4229887c99309
Timofey Mysko <myskotima130@gmail.com>
2024-05-26 16:47:11 +0300

    create product page

---
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function ProductForm() {
+  return (
+    <form>
+      <div>
+        <Label></Label>
+        <Input />
+      </div>
+    </form>
+  );
+}
