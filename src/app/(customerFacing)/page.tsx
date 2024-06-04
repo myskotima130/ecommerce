68911a572c0ae43437598f43a6623f2970caf90d
Timofey Mysko <myskotima130@gmail.com>
2024-06-04 14:45:08 -0700

    add product card

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,4 +1,9 @@
+import { ProductCard } from "@/components/ProductCard";
+import { Button } from "@/components/ui/button";
 import db from "@/db/db";
+import { Product } from "@prisma/client";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 function getMostPopularProducts() {
   return db.product.findMany({
@@ -18,4 +23,43 @@ function getNewestProducts() {
   });
 }
 
-export default function Home() {}
+export default function Home() {
+  return (
+    <main className="space-y-12">
+      <ProductGridSection
+        title="Most Popular"
+        productsFetcher={getMostPopularProducts}
+      />
+      <ProductGridSection title="Newest" productsFetcher={getNewestProducts} />
+    </main>
+  );
+}
+
+type ProductGridSectionProps = {
+  title: string;
+  productsFetcher: () => Promise<Product[]>;
+};
+
+async function ProductGridSection({
+  productsFetcher,
+  title,
+}: ProductGridSectionProps) {
+  return (
+    <div className="space-y-4">
+      <div className="flex gap-4">
+        <h2 className="text-3xl font-bold">{title}</h2>
+        <Button variant="outline" asChild>
+          <Link href="/products" className="space-x-2">
+            <span>See All Products</span>
+            <ArrowRight className="size-4" />
+          </Link>
+        </Button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {(await productsFetcher()).map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </div>
+    </div>
+  );
+}
